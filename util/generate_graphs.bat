9db25507bfd09616bfc612923cda0839d83af8e8
Orwellian <orwellian225@gmail.com>
2023-05-30 22:45:09 +0200

    add: Graph Generation Scripts

---
+++ util/generate_graphs.bat
@@ -0,0 +1,5 @@
+python util/graph_generator.py 3 data/OPT_BC.csv data/OPT_WC.csv data/OPT_RANDOM.csv "Optimised Algorithm Case Comparison"
+python util/graph_generator.py 3 data/BF_BC.csv data/BF_WC.csv data/BF_RANDOM.csv "Brute Force Algorithm Case Comparison"
+python util/graph_generator.py 2 data/OPT_BC.csv data/BF_BC.csv "Brute Force vs Optimised Algorithm Best Case"
+python util/graph_generator.py 2 data/OPT_WC.csv data/BF_WC.csv "Brute Force vs Optimised Algorithm Worst Case"
+python util/graph_generator.py 2 data/OPT_RANDOM.csv data/BF_RANDOM.csv "Brute Force vs Optimised Algorithm Random Case"
